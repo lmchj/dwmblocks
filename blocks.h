f277445fdd8fc3aa50162879b30cb8eed0438ddc
arch-lm <lmchj@pm.me>
2020-07-07 21:27:30 -0600

    Added and commented block for currencies exchanges. To fix.

--- blocks.h
+++ blocks.h
@@ -1,9 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "~/.local/bin/statusbar/wttr",						1800,	5},
-	{"", "~/.local/bin/statusbar/cal",						60,		0},
+	/*{"", "~/.local/bin/statusbar/currency",	14400, 5},*/
+	{"", "~/.local/bin/statusbar/wttr",	1800, 5},
+	{"", "~/.local/bin/statusbar/cal", 60, 1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim = '|';
+static char delim = ' ';
