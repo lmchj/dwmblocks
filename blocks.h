e004d6dcd21478f9dd50336639731cb19dd1d7e5
arch-lm <lmchj@pm.me>
2020-07-19 11:31:30 -0600

    Clickable modules.

--- blocks.h
+++ blocks.h
@@ -2,9 +2,10 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	/*{"", "~/.local/bin/statusbar/currency",	14400, 5},*/
+	{"", "~/.local/bin/statusbar/music",	0, 11},
 	{"", "~/.local/bin/statusbar/wttr",	1800, 5},
 	{"", "~/.local/bin/statusbar/cal", 60, 1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim = ' ';
+static char *delim = " ";
--- dwmblocks.c
+++ dwmblocks.c
@@ -13,8 +13,10 @@ typedef struct {
 	unsigned int interval;
 	unsigned int signal;
 } Block;
-void dummysighandler(int num);
 void sighandler(int num);
+void buttonhandler(int sig, siginfo_t *si, void *ucontext);
+void replace(char *str, char old, char new);
+void remove_all(char *str, char to_remove);
 void getcmds(int time);
 #ifndef __OpenBSD__
 void getsigcmds(int signal);
@@ -37,9 +39,35 @@ static char statusstr[2][256];
 static int statusContinue = 1;
 static void (*writestatus) () = setroot;
 
+void replace(char *str, char old, char new)
+{
+	int N = strlen(str);
+	for(int i = 0; i < N; i++)
+		if(str[i] == old)
+			str[i] = new;
+}
+
+void remove_all(char *str, char to_remove) {
+	char *read = str;
+	char *write = str;
+	while (*read) {
+		if (*read == to_remove) {
+			read++;
+			*write = *read;
+		}
+		read++;
+		write++;
+	}
+}
+
 //opens process *cmd and stores output in *output
 void getcmd(const Block *block, char *output)
 {
+	if (block->signal)
+	{
+		output[0] = block->signal;
+		output++;
+	}
 	strcpy(output, block->icon);
 	char *cmd = block->command;
 	FILE *cmdf = popen(cmd,"r");
@@ -47,10 +75,12 @@ void getcmd(const Block *block, char *output)
 		return;
 	char c;
 	int i = strlen(block->icon);
-	fgets(output+i, CMDLENGTH-i, cmdf);
+	fgets(output+i, CMDLENGTH-(strlen(delim)+1), cmdf);
+	remove_all(output, '\n');
 	i = strlen(output);
-	if (delim != '\0' && --i)
-		output[i++] = delim;
+    if ((i > 0 && block != &blocks[LENGTH(blocks) - 1]))
+        strcat(output, delim);
+    i+=strlen(delim);
 	output[i++] = '\0';
 	pclose(cmdf);
 }
@@ -80,15 +110,23 @@ void getsigcmds(int signal)
 
 void setupsignals()
 {
-    /* initialize all real time signals with dummy handler */
-    for(int i = SIGRTMIN; i <= SIGRTMAX; i++)
-        signal(i, dummysighandler);
-
+	struct sigaction sa;
 	for(int i = 0; i < LENGTH(blocks); i++)
 	{
 		if (blocks[i].signal > 0)
+		{
 			signal(SIGRTMIN+blocks[i].signal, sighandler);
+			sigaddset(&sa.sa_mask, SIGRTMIN+blocks[i].signal);
+		}
 	}
+	sa.sa_sigaction = buttonhandler;
+	sa.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &sa, NULL);
+	struct sigaction sigchld_action = {
+  		.sa_handler = SIG_DFL,
+  		.sa_flags = SA_NOCLDWAIT
+	};
+	sigaction(SIGCHLD, &sigchld_action, NULL);
 
 }
 #endif
@@ -97,8 +135,11 @@ int getstatus(char *str, char *last)
 {
 	strcpy(last, str);
 	str[0] = '\0';
-	for(int i = 0; i < LENGTH(blocks); i++)
+    for(int i = 0; i < LENGTH(blocks); i++) {
 		strcat(str, statusbar[i]);
+        if (i == LENGTH(blocks) - 1)
+            strcat(str, " ");
+    }
 	str[strlen(str)-1] = '\0';
 	return strcmp(str, last);//0 if they are the same
 }
@@ -143,19 +184,35 @@ void statusloop()
 }
 
 #ifndef __OpenBSD__
-/* this signal handler should do nothing */
-void dummysighandler(int signum)
-{
-    return;
-}
-#endif
-
-#ifndef __OpenBSD__
 void sighandler(int signum)
 {
 	getsigcmds(signum-SIGRTMIN);
 	writestatus();
 }
+
+void buttonhandler(int sig, siginfo_t *si, void *ucontext)
+{
+	char button[2] = {'0' + si->si_value.sival_int & 0xff, '\0'};
+	sig = si->si_value.sival_int >> 8;
+	if (fork() == 0)
+	{
+		const Block *current;
+		for (int i = 0; i < LENGTH(blocks); i++)
+		{
+			current = blocks + i;
+			if (current->signal == sig)
+				break;
+		}
+		char *command[] = { "/bin/sh", "-c", current->command, NULL };
+		setenv("BLOCK_BUTTON", button, 1);
+		setsid();
+		execvp(command[0], command);
+		exit(EXIT_SUCCESS);
+	}
+	getsigcmds(sig);
+	writestatus();
+}
+
 #endif
 
 void termhandler(int signum)
@@ -169,7 +226,7 @@ int main(int argc, char** argv)
 	for(int i = 0; i < argc; i++)
 	{
 		if (!strcmp("-d",argv[i]))
-			delim = argv[++i][0];
+			delim = argv[++i];
 		else if(!strcmp("-p",argv[i]))
 			writestatus = pstdout;
 	}
