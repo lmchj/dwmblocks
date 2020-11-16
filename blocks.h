1814f4aab69a67629b108a2fef9088fe9ca51158
nosike <elem@lmcj.xyz>
2020-11-15 22:30:04 -0700

    battery, network, volume added

--- blocks.h
+++ blocks.h
@@ -2,10 +2,13 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	/*{"", "~/.local/bin/statusbar/currency",	14400, 5},*/
-	{"", "~/.local/bin/statusbar/music",	0, 11},
 	{"", "~/.local/bin/statusbar/wttr",	1800, 5},
+	{"", "~/.local/bin/statusbar/volume",	0, 10},
+	{"", "~/.local/bin/statusbar/network", 5, 4},
+	{"", "~/.local/bin/statusbar/battery", 60, 1},
 	{"", "~/.local/bin/statusbar/cal", 60, 1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char *delim = " ";
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
